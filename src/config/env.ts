bb2032527d9ad47814fca54a1b12b13f9d7a0cda
Bima42 <tanpau42@gmail.com>
2026-01-31 22:45:40 +0100

    feat: setup drizzlr

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,21 +3,19 @@ import { z } from 'zod';
 
 export const env = createEnv({
     server: {
+        DATABASE_URL: z.string().min(1, 'Database URL is required'),
         NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     },
     client: {
         NEXT_PUBLIC_APP_URL: z.url(),
-        NEXT_PUBLIC_GOOGLE_MAPS_KEY: z.string().min(1, 'Google Maps API key is required'),
-        NEXT_PUBLIC_GOOGLE_MAP_ID: z.string().min(1, 'Google Maps ID is required'),
     },
     runtimeEnv: {
         // Server
+        DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
 
         // Client
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-        NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
-        NEXT_PUBLIC_GOOGLE_MAP_ID: process.env.NEXT_PUBLIC_GOOGLE_MAP_ID,
     },
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
     emptyStringAsUndefined: true
