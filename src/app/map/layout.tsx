ddcbcca1a4d62f24fe86d89017332a65f75143cd
Bima42 <tanpau42@gmail.com>
2026-01-31 22:28:35 +0100

    chore: clear map components

--- src/app/map/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function MapLayout({ children }: { children: React.ReactNode }) {
-  return <div className="h-screen w-screen overflow-hidden">{children}</div>;
-}
--- src/components/map/map-3d/index.ts
+++
@@ -1,3 +0,0 @@
-export { Map3D } from "./map-3d";
-export { Marker3D } from "./marker-3d";
-export type { Map3DCameraProps, Map3DProps } from "./types";
